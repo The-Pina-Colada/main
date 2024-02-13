1b7004dca206187ffe65c1689e6b8297523bf088
xnousnow <xnou.snow@gmail.com>
2024-02-13 20:54:38 +0900

    Create dictionary for item icons

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -10,4 +10,9 @@ export interface Item {
   y: number
 }
 
+const icons = {
+  enchanted_book: 'https://minecraft.wiki/images/Enchanted_Book.gif',
+  totem_of_undying: 'https://minecraft.wiki/images/Totem_of_Undying_JE2_BE2.png'
+}
+
 export const data = writable<Item[]>()
