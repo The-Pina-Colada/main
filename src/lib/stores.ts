0d4ebe79858c0f3bb0b5126945f05bdc6c7bf772
xnousnow <xnou.snow@gmail.com>
2024-02-14 14:49:09 +0900

    Get store info from the server

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -1,7 +1,7 @@
 import { writable } from 'svelte/store'
 import { supabase } from '$lib/supabase'
 
-export interface Item {
+export interface ProcessedItem {
   icon: string
   name?: string
   price: string | number
@@ -10,21 +10,48 @@ export interface Item {
   x: number
   y: number
 }
+interface RawItem {
+  icon: string
+  name?: string
+  price: string | number
+  store: string
+}
+
+interface Icon {
+  name: string
+  url: string
+}
+
+interface Store {
+  name: string
+  owner: string
+  x: number
+  y: number
+}
 
 export const loading = writable(true)
 
-export const shopitems = writable<Item[]>([])
+export const shopitems = writable<ProcessedItem[]>([])
 ;(async () => {
-  const { data: itemsData } = await supabase.from('shopitems').select()
-  const { data: iconsData } = await supabase.from('itemimages').select()
+  const { data: itemsData  } = await supabase.from('shopitems').select() as { data: RawItem[] }
+  const { data: iconsData } = await supabase.from('itemimages').select() as { data: Icon[] }
+  const { data: storesData } = await supabase.from('stores').select()  as { data: Store[] }
 
-  const iconswapped = itemsData?.map((item: Item) => {
+  const processed = itemsData?.map((item) => {
+    const icon = iconsData?.find((icon) => icon.name === item.icon)
+    const store = storesData?.find((store) => store.name === item.store)
+
     return {
-      ...item,
-      icon: iconsData?.find((icon) => icon.name === item.icon)?.url ?? item.icon
+      icon: icon?.url,
+      name: item.name,
+      price: item.price,
+      seller: store?.owner,
+      store: store?.name,
+      x: store?.x,
+      y: store?.y
     }
   })
 
-  shopitems.set(iconswapped ?? [])
+  shopitems.set(processed as ProcessedItem[])
   loading.set(false)
 })()
